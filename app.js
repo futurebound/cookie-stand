364c1e459b05bc8773d38084d0b41a5276234fc5
Mitchell M <mitchellm1618@gmail.com>
2017-10-24 21:33:45 -0700

    added constructor function, deleted lots of redundant code

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var table = document.getElementById('salesContent');
+var stores = [];
+
 function Store(storeName, minCust, maxCust, avgPerCust) {
   this.storeName = storeName,
   this.minCust = minCust,
@@ -9,11 +12,25 @@ function Store(storeName, minCust, maxCust, avgPerCust) {
   this.projectedHourlySales = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM',];
 };
 
-var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
-var seatac = new Store('SeaTac Airport', 3, 24, 1.2);
-var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
-var capHill = new Store('Capitol Hill', 20, 38, 2.3);
-var alki = new Store('Alki', 2, 16, 4.6);
+stores.push(new Store('1st and Pike', 23, 65, 6.3));
+stores.push(new Store('SeaTac Airport', 3, 24, 1.2));
+stores.push(new Store('Seattle Center', 11, 38, 3.7));
+stores.push(new Store('Capitol Hill', 20, 38, 2.3));
+stores.push(new Store('Alki', 2, 16, 4.6));
+
+hourlyCustomers: function() {
+  var min = this.minCust;
+  var max = this.maxCust;
+  var avg = this.avgPerCust;
+  for(var i = 0; i < this.storeHours.length; i++) {
+    var projectedCustomers = Math.floor(Math.random() * (max - min) + min);
+    this.projectedHourlySales.push(Math.ceil(projectedCustomers * avg));
+    this.totalHourlySales += (Math.ceil(projectedCustomers * avg));
+    console.log('projected hourly sales:', this.projectedHourlySales[i]);
+    console.log('total hourly sales:', this.totalHourlySales);
+  };
+  return [this.projectedHourlySales, this.totalHourlySales];
+};
 
 
 var pikeStore = {
@@ -45,129 +62,33 @@ var pikeStore = {
   }
 };
 
-var seatacStore = {
-  storeName: 'SeaTac Airport',
-  minCust: 3,
-  maxCust: 24,
-  avgPerCust: 1.2,
-  totalHourlySales: 0,
-  projectedHourlySales: [],
-  storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm,', '5pm', '6pm', '7pm'],
-  hourlyCustomers: function() {
-    var min = this.minCust;
-    var max = this.maxCust;
-    var avg = this.avgPerCust;
-    for(var i = 0; i < this.storeHours.length; i++) {
-      var projectedCustomers = Math.floor(Math.random() * (max - min) + min);
-      this.projectedHourlySales.push(Math.ceil(projectedCustomers * avg));
-      this.totalHourlySales += (Math.ceil(projectedCustomers * avg));
-      console.log('projected hourly sales:', this.projectedHourlySales[i]);
-      console.log('total hourly sales:', this.totalHourlySales);
-    };
-    return [this.projectedHourlySales, this.totalHourlySales];
-  }
-};
-
-var seattleCenterStore = {
-  storeName: 'Seattle Center',
-  minCust: 11,
-  maxCust: 38,
-  avgPerCust: 3.7,
-  totalHourlySales: 0,
-  projectedHourlySales: [],
-  storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm,', '5pm', '6pm', '7pm'],
-  hourlyCustomers: function() {
-    var min = this.minCust;
-    var max = this.maxCust;
-    var avg = this.avgPerCust;
-    for(var i = 0; i < this.storeHours.length; i++) {
-      var projectedCustomers = Math.floor(Math.random() * (max - min) + min);
-      this.projectedHourlySales.push(Math.ceil(projectedCustomers * avg));
-      this.totalHourlySales += (Math.ceil(projectedCustomers * avg));
-      console.log('projected hourly sales:', this.projectedHourlySales[i]);
-      console.log('total hourly sales:', this.totalHourlySales);
-    };
-    return [this.projectedHourlySales, this.totalHourlySales];
-  }
-};
-
-var capHillStore = {
-  storeName: 'Capitol Hill',
-  minCust: 20,
-  maxCust: 38,
-  avgPerCust: 2.3,
-  totalHourlySales: 0,
-  projectedHourlySales: [],
-  storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm,', '5pm', '6pm', '7pm'],
-  hourlyCustomers: function() {
-    var min = this.minCust;
-    var max = this.maxCust;
-    var avg = this.avgPerCust;
-    for(var i = 0; i < this.storeHours.length; i++) {
-      var projectedCustomers = Math.floor(Math.random() * (max - min) + min);
-      this.projectedHourlySales.push(Math.ceil(projectedCustomers * avg));
-      this.totalHourlySales += (Math.ceil(projectedCustomers * avg));
-      console.log('projected hourly sales:', this.projectedHourlySales[i]);
-      console.log('total hourly sales:', this.totalHourlySales);
-    };
-    return [this.projectedHourlySales, this.totalHourlySales];
-  }
-};
-
-var alkiStore = {
-  storeName: 'Alki',
-  minCust: 2,
-  maxCust: 16,
-  avgPerCust: 4.6,
-  totalHourlySales: 0,
-  projectedHourlySales: [],
-  storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm,', '5pm', '6pm', '7pm'],
-  hourlyCustomers: function() {
-    var min = this.minCust;
-    var max = this.maxCust;
-    var avg = this.avgPerCust;
-    for(var i = 0; i < this.storeHours.length; i++) {
-      var projectedCustomers = Math.floor(Math.random() * (max - min) + min);
-      this.projectedHourlySales.push(Math.ceil(projectedCustomers * avg));
-      this.totalHourlySales += (Math.ceil(projectedCustomers * avg));
-      console.log('projected hourly sales:', this.projectedHourlySales[i]);
-      console.log('total hourly sales:', this.totalHourlySales);
-    };
-    return [this.projectedHourlySales, this.totalHourlySales];
-  }
-};
-
-// array with all of the previously defined object literals
-var storesArray = [pikeStore, seatacStore, seattleCenterStore, capHillStore, alkiStore];
-
-
 // salesMetrics is a function that accepts storesArray as argument
-var salesMetrics = function(storesArray) {
+var salesMetrics = function(storeData) {
   // cycles through each store name and for each
   // start for loop - iterate through storesArray
-  for (var j = 0; j < storesArray.length; j++) {
+  for (var j = 0; j < storeData.length; j++) {
     // creates section elements in HTML
     // build DOM elements
     // storeSection is for creating a 5 different sections, 1 for each item salesAr
     document.createElement('section');
     // var storeName = '<h2>' + storesArray[k].name + '</h2>';
     // setup salesAmt array - this will get data pushed to it
-    var salesAmount = ['<h2>' + storesArray[j].storeName + '</h2>', '<ul>'];
+    var salesAmount = ['<h2>' + storeData[j].storeName + '</h2>', '<ul>'];
     // defines salesAmountTotal to utilized within the following code and nested for loop
     var salesAmountTotal;
     // grab current sales item and call hourlyCustomers function to generate hourly sales
-    storesArray[j].hourlyCustomers();
+    storeData[j].hourlyCustomers();
     // for every storeName that has had a section created, also creates an unordered list
     var storeSales = document.createElement('section');
     // nested for loop that will create <li> in HTML for every iteration of the businessHours array within every iteration of the storesArray. this allows it to cycle and run for every different storesection, which was already in an array and linked to the above object literals i.e. storesArray[0].hoursOfBusiness.length will crank out 5 different sections, and in each iteration it runs this nested loop
-    for (var k = 0; k < storesArray[j].storeHours.length; k++) {
+    for (var k = 0; k < storeData[j].storeHours.length; k++) {
       // this pushed the salesAmount array which is an array with the h2 header strings that contain the name of the store, which changes with each iteration of the above for loop to match the proper iteration index
       // what is pushed is ultimately 14 list items from this loop that contain the hour of business (6AM, 7AM, etc.) as well as the randomly generated sales from the above storesArray[k].hourlyCustomers(); function call, in one giant concatenated string that closes itself as a list time and is placed in the unordered list created for each section and store above
       // console.log(storesArray[j]);
-      salesAmount.push('<li>' + storesArray[j].storeHours[k] + ': ' + storesArray[j].projectedHourlySales[k] + '</li>');
+      salesAmount.push('<li>' + storeData[j].storeHours[k] + ': ' + storeData[j].projectedHourlySales[k] + '</li>');
     }
     // this assigns a value to the previously defined salesAmountTotal variable, which is a list item placed at the bottom of the previous list saying 'total: ' then the sales total for the hours of operation
-    salesAmountTotal = '<li>Total: ' + storesArray[j].totalHourlySales + '</li>';
+    salesAmountTotal = '<li>Total: ' + storeData[j].totalHourlySales + '</li>';
     // this pushes the list item string to the salesAmount array above
     salesAmount.push(salesAmountTotal);
     // this pushes a closing </ul> tag to close the unordered list
@@ -182,36 +103,4 @@ var salesMetrics = function(storesArray) {
 };
 
 // this runs the buildSales function and accepts the storesArray array as its argument
-salesMetrics(storesArray);
-
-
-
-
-
-
-
-
-
-
-
-//
-// var buildSales = function(salesArray) {
-//   for (var k = 0; k < salesArray.length; k++) {
-//     document.createElement('section');
-//     var salesAmount = ['<h2>' + salesArray[k].name + '</h2>', '<ul>'];
-//     var salesAmountTotal;
-//     salesArray[k].hourlyCustomers();
-//     var storeSales = document.createElement('section');
-//     for (var l = 0; l < salesArray[k].hoursOfBusiness.length; l++) {
-//       salesAmount.push('<li>' + salesArray[k].hoursOfBusiness[l] + ': ' + salesArray[k].salesPerHour[l] + '</li>');
-//     }
-//     salesAmountTotal = '<li>Total: ' + salesArray[k].salesTotal + '</li>';
-//     salesAmount.push(salesAmountTotal);
-//     salesAmount.push('</ul>');
-//     storeSales.innerHTML = salesAmout.join('');
-//     console.log('store sales:', storeSales);
-//     document.getElementById('salesContent').appendChild(storeSales);
-//   }
-// };
-//
-// buildSales(storeLocations);
+salesMetrics(stores);
